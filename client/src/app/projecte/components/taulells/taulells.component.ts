1c90a4f823e1db341ae6ebf273e5da257565d39c
janEstrada24 <jestrada48@boscdelacoma.cat>
2023-03-14 19:01:23 +0100

    ara Examen

--- client/src/app/projecte/components/taulells/taulells.component.ts
+++ client/src/app/projecte/components/taulells/taulells.component.ts
@@ -12,14 +12,56 @@ export class TaulellsComponent {
 
   caselles!: ICasella[][];
   taulell: Taulell = new Taulell(0);
+  taulell2: Taulell = new Taulell(0);
   pecesBlanques: IPeca[] = [];
   pecesNegres: IPeca[] = [];
 
-  constructor() {
+  draggIndex: number = 0;
 
+  constructor() {
+    this.taulell.inicialitzarCasellesAmbPeces1();
+    this.taulell2.inicialitzarCasellesAmbPeces2();
     console.log(this.taulell);
     console.log("taulell construit");
   }
-
+/*
+  dragStart(event: DragEvent) {
+    // Almacenar la imagen que se está arrastrando
+    event.dataTransfer.setData('text/plain', event.target.id);
+    event.currentTarget.classList.add('dragging');
+  }
+  
+  dragEnd(event: DragEvent) {
+    // Eliminar la clase dragging
+    event.currentTarget.classList.remove('dragging');
+  }
+  
+  dragOver(event: DragEvent) {
+    // Prevenir el comportamiento predeterminado
+    event.preventDefault();
+  }
+  
+  dragEnter(event: DragEvent) {
+    // Agregar una clase a la tabla cuando la imagen se acerca a ella
+    event.currentTarget.classList.add('dragover');
+  }
+  
+  dragLeave(event: DragEvent) {
+    // Eliminar la clase dragover cuando la imagen se aleja de la tabla
+    event.currentTarget.classList.remove('dragover');
+  }
+  
+  drop(event: DragEvent) {
+    // Prevenir el comportamiento predeterminado
+    event.preventDefault();
+  
+    // Obtener la imagen que se está moviendo
+    const imagenId = event.dataTransfer.getData('text/plain');
+    const imagen = document.getElementById(imagenId);
+  
+    // Mover la imagen a la nueva posición
+    event.currentTarget.appendChild(imagen);
+  }
+*/
 
 }
--- client/src/app/projecte/models/Entities/Implementation/Taulell.ts
+++ client/src/app/projecte/models/Entities/Implementation/Taulell.ts
@@ -13,78 +13,31 @@ export class Taulell implements ITaulell<IPeca> {
     constructor (num: number) {
         this.num = num;
         this.caselles = new Array<Array<ICasella>>();
-        this.inicialitzarCasellesAmbPeces();
+        //this.inicialitzarCasellesAmbPeces1();
     }    
 
-    public inicialitzarCaselles(): void {
-        for (let i = 1; i < 9; i++) {
-            this.caselles[i] = [];
-            for (let j = 1; j < 9; j++) {
-                console.log("i = " + i + " j = " + j);
-                this.caselles[i][j] = new Casella (
-                                        String.fromCharCode(65 + j - 1), 
-                                        i + 1, 
-                                        (i + j) % 2 == 0 ? "#e3dc9f" : "#ff9900", 
-                                        new Peca("", "") 
-                                    );
-                console.log(this.caselles[i][j]);
-            }
-        }
-    }
-
-    public inicialitzarCasellesAmbPeces(): void {
+    public inicialitzarCasellesAmbPeces1(): void {
         console.log("inicialitzarCasellesAmbPeces");
         for (let i = 0; i < 8; i++) {
             this.caselles[i] = [];
-            /*for (let j = 0; j < 8; j++) {
-               console.log("i = " + i + " j = " + j);
-                if (i == 0) {
-                    this.caselles[i][j] = new Casella (
-                        String.fromCharCode(65 + j - 1), 
-                        i + 1, 
-                        (i + j) % 2 == 0 ? "#e3dc9f" : "#ff9900", 
-                        new Peca(peces[i]+"Blanc.png","blanca")
-                    );
-                }
-                else if (i == 1) {
-                    this.caselles[i][j] = new Casella (
-                        String.fromCharCode(65 + j - 1), 
-                        i + 1, 
-                        (i + j) % 2 == 0 ? "#e3dc9f" : "#ff9900", 
-                        new Peca("peoBlanc.png","blanca")
-                    );
-                }
-                else if (i == 7) {
-                    this.caselles[i][j] = new Casella (
-                        String.fromCharCode(65 + j - 1), 
-                        i + 1, 
-                        (i + j) % 2 == 0 ? "#e3dc9f" : "#ff9900", 
-                        new Peca("peoNegre.png","negra")
-                    );
-                }
-                else if (i == 8) {
-                    this.caselles[i][j] = new Casella (
-                        String.fromCharCode(65 + j - 1), 
-                        i + 1, 
-                        (i + j) % 2 == 0 ? "#e3dc9f" : "#ff9900", 
-                        new Peca(peces[i]+"Negre.png","negra")
-                    );
-                } else {
-                    this.caselles[i][j] = new Casella (
-                        String.fromCharCode(65 + j - 1), 
-                        i, 
-                        (i + j) % 2 == 0 ? "#e3dc9f" : "#ff9900", 
-                        new Peca("", "") 
-                    );
-                }
-                console.log(this.caselles[i][j]);
-
-            }*/
         }
         this.posarPeces(0,"Blanc.png","blanca");
         this.posarPeces(7,"Negre.png","negra");
         this.posarPeons(1,"peoBlanc.png","blanques");
         this.posarPeons(6,"peoNegre.png","negres");
+
+        this.crearCasellesBuides();
+    }
+
+    public inicialitzarCasellesAmbPeces2(): void {
+        console.log("inicialitzarCasellesAmbPeces");
+        for (let i = 0; i < 8; i++) {
+            this.caselles[i] = [];
+        }
+        this.posarPeces(0,"Negre.png","negra");
+        this.posarPeces(7,"Blanc.png","blanca");
+        this.posarPeons(1,"peoNegre.png","negres");
+        this.posarPeons(6,"peoBlanc.png","blanques");
 
         this.crearCasellesBuides();
     }
@@ -123,7 +76,6 @@ export class Taulell implements ITaulell<IPeca> {
     public posarPeons(posicioFila: number, cadenaColor: string, color: string) {
         
         for (let j = 0; j < 8; j++) {
-
             this.caselles[posicioFila][j] = new Casella (
                                                 String.fromCharCode(65 + j - 1), 
                                                 posicioFila + 1, 
@@ -134,57 +86,4 @@ export class Taulell implements ITaulell<IPeca> {
 
     }
 
-    public crearPeces(): void {
-        const peces = ["/assets/peces/torre", "/assets/peces/alfil", "/assets/peces/cavall", "/assets/peces/reina", 
-                        "/assets/peces/rei", "/assets/peces/cavall", "/assets/peces/alfil", "/assets/peces/torre"];
-
-        for (let i = 1; i < 9; i++) {
-            this.caselles[i] = [];
-
-            for (let j = 1; j < 9; j++) {
-
-                if (i == 1) {
-                    this.caselles[i][j].peca = new Peca(peces[i]+"Blanc.png","blanca");
-                }
-                else if (i == 2) {
-                    this.caselles[i][j].peca = new Peca("peoBlanc.png","blanca");
-                }
-                else if (i == 8) {
-                    this.caselles[i][j].peca = new Peca("peoNegre.png","negra");
-                }
-                else if (i == 9) {
-                    this.caselles[i][j].peca = new Peca(peces[i]+"Negre.png","negra");
-                }
-            }
-        }
-
-        /*this.pecesBlanques.push(new Peca(2, "/assets/pecesBlanques/reinaBlanca.png", "blanca"));
-        this.pecesBlanques.push(new Peca(3, "/assets/pecesBlanques/torreBlanca.png", "blanca"));
-        this.pecesBlanques.push(new Peca(4, "/assets/pecesBlanques/torreBlanca.png", "blanca"));
-        this.pecesBlanques.push(new Peca(5, "../../../../../assets/pecesBlanques/alfilBlanc.png", "blanca"));
-        this.pecesBlanques.push(new Peca(6, "../../../../../assets/pecesBlanques/alfilBlanc.png", "blanca"));
-        this.pecesBlanques.push(new Peca(7, "../../../../../assets/pecesBlanques/cavallBlanc.png", "blanca"));
-        this.pecesBlanques.push(new Peca(8, "../../../../../assets/pecesBlanques/cavallBlanc.png", "blanca"));
-
-        for (let i = 9; i < 16; i++) {
-            this.pecesBlanques.push(new Peca(i, "../../../../../assets/pecesBlanques/peoBlanc.png", "blanca"));
-        }*/
-    }
-
-    public crearPecesNegres(fila: Array<Casella>): void {
-        /*this.pecesNegres.push(new Peca(1, "../../../../../assets/pecesNegres/reiNegre.png", "negra"));
-        this.pecesNegres.push(new Peca(2, "../../../../../assets/pecesNegres/reinaNegre.png", "negra"));
-        this.pecesNegres.push(new Peca(3, "../../../../../assets/pecesNegres/torreNegre.png", "negra"));
-        this.pecesNegres.push(new Peca(4, "../../../../../assets/pecesNegres/torreNegre.png", "negra"));
-        this.pecesNegres.push(new Peca(5, "../../../../../assets/pecesNegres/alfilNegre.png", "negra"));
-        this.pecesNegres.push(new Peca(6, "../../../../../assets/pecesNegres/alfilNegre.png", "negra"));
-        this.pecesNegres.push(new Peca(7, "../../../../../assets/pecesNegres/cavallNegre.png", "negra"));
-        this.pecesNegres.push(new Peca(8, "../../../../../assets/pecesNegres/cavallNegre.png", "negra"));
-*/
-
-        for (let i = 0; i < 8; i++) {
-          
-            fila[i].peca = new Peca("/assets/pecesNegres/peoNegre.png", "negra");
-        }
-    }
 }
